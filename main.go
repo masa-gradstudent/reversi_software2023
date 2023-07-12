c53e41712cd4b424c4287e1834ab6f167943eece
upnt <upnt.github@gmail.com>
2023-07-12 02:16:27 +0000

    defalut

--- main.go
+++ main.go
@@ -9,3 +9,12 @@ func create_board() Board {
 	b.tokens = [8][8]int{}
 	return b
 }
+
+func defalut_board() Board {
+	b := create_board()
+	b.tokens[3][3] = 1
+	b.tokens[3][4] = -1
+	b.tokens[4][3] = -1
+	b.tokens[4][4] = 1
+	return b
+}
