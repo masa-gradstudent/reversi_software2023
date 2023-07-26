74c67ab37636333f0d8a6352a37421b7ffcacbb5
kakushinjyaku <kaku.shinjyaku@gmail.com>
2023-07-26 00:21:05 +0000

    123

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
+const (
+	WHITE = 1
+	BLACK = -1
+)
+
 type Board struct {
-	tokens [8][8]int
+	tokens      [8][8]int
+	currentTurn int
 }
 
 func create_board() Board {
@@ -12,9 +18,55 @@ func create_board() Board {
 
 func defalut_board() Board {
 	b := create_board()
-	b.tokens[3][3] = 1
-	b.tokens[3][4] = -1
-	b.tokens[4][3] = -1
-	b.tokens[4][4] = 1
+	b.tokens[3][3] = WHITE
+	b.tokens[3][4] = BLACK
+	b.tokens[4][3] = BLACK
+	b.tokens[4][4] = WHITE
 	return b
+}
+func (b *Board) count() (int, int) {
+	white := 0
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8; j++ {
+			if b.tokens[i][j] == WHITE {
+				white += 1
+			}
+		}
+	}
+	return white, 0
+}
+
+func (b *Board) flip(row int, column int) {
+	b.flip_forward(row, column, 1)
+	b.flip_forward(row, column, -1)
+}
+
+func (b *Board) flip_forward(row int, column int, forward int) {
+	color := b.tokens[row][column]
+	flip_list := make([]int, 0, 8)
+	for {
+		row += forward
+		if 0 > row || row >= 8 {
+			break
+		}
+		ocolor := b.tokens[row][column]
+		if -ocolor == color {
+			flip_list = append(flip_list, row)
+		} else {
+			break
+		}
+	}
+	for _, i := range flip_list {
+		b.tokens[i][column] = color
+	}
+}
+
+func determineWinner(white, black int) string {
+	if white > black {
+		return "White wins!"
+	} else if black > white {
+		return "Black wins!"
+	} else {
+		return "It's a tie!"
+	}
 }
--- main_test.go
+++ main_test.go
@@ -41,3 +41,64 @@ func TestDefalut(t *testing.T) {
 
 	}
 }
+func TestCount01(t *testing.T) {
+	b := create_board()
+	white, black := b.count()
+	if white != 0 || black != 0 {
+		t.Errorf("Count Error")
+	}
+}
+
+func TestCount02(t *testing.T) {
+	b := create_board()
+	b.tokens[0][0] = WHITE
+	b.tokens[0][1] = WHITE
+	b.tokens[0][2] = WHITE
+	white, black := b.count()
+	if white != 3 || black != 0 {
+		t.Errorf("Count Error")
+	}
+}
+
+func TestFlip(t *testing.T) {
+	b := create_board()
+	b.tokens[2][3] = BLACK
+	b.tokens[3][3] = WHITE
+	b.tokens[4][3] = WHITE
+	b.tokens[5][3] = WHITE
+	b.tokens[6][3] = BLACK
+	b.flip(6, 3)
+	expected := [8][8]int{
+		{0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, BLACK, 0, 0, 0, 0},
+		{0, 0, 0, BLACK, 0, 0, 0, 0},
+		{0, 0, 0, BLACK, 0, 0, 0, 0},
+		{0, 0, 0, BLACK, 0, 0, 0, 0},
+		{0, 0, 0, BLACK, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0},
+	}
+
+	if !reflect.DeepEqual(b.tokens, expected) {
+		t.Errorf("Flip Error: %d", b.tokens)
+	}
+}
+func TestDetermineWinner(t *testing.T) {
+	white1, black1 := 20, 15
+	result1 := determineWinner(white1, black1)
+	if result1 != "White wins!" {
+		t.Errorf("Test case 1 failed: Expected 'White wins!', got '%s'", result1)
+	}
+
+	white2, black2 := 12, 18
+	result2 := determineWinner(white2, black2)
+	if result2 != "Black wins!" {
+		t.Errorf("Test case 2 failed: Expected 'Black wins!', got '%s'", result2)
+	}
+
+	white3, black3 := 30, 30
+	result3 := determineWinner(white3, black3)
+	if result3 != "It's a tie!" {
+		t.Errorf("Test case 3 failed: Expected 'It's a tie!', got '%s'", result3)
+	}
+}
